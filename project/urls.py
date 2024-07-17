1ce3a661b631eaef3e62718afe16f63be3aff8fd
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2024-07-17 15:38:43 +0300

    Update Python

--- project/urls.py
+++ project/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls',namespace='home')),
     path('api-auth/', include('rest_framework.urls'))
+    
 
     
     
